17920aa0b654c95d71f73fd908ee40fb6b9f43d1
rlongdragon <jz744335@gmail.com>
2024-10-30 06:42:53 +0000

    week7

---
+++ week7/1. MaxPool1D/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+float findMax(float originalInput[50], int startIndex, int endIndex) {
+  startIndex = startIndex < 0 ? 0 : startIndex;
+  endIndex = endIndex > 50 ? 50 : endIndex;
+
+  float max = originalInput[startIndex];
+  for (int i = startIndex; i < endIndex; i++) {
+    if (originalInput[i] > max) {
+      max = originalInput[i];
+    }
+  }
+  return max;
+}
+
+int main() {
+  int n, padding, poolSize, strike;
+  float originalInput[50];
+  int count = 0;
+
+  scanf("%d%d%d%d", &n, &padding, &poolSize, &strike);
+
+  for (int i = 0; i < n; i++) {
+    scanf("%f", &originalInput[i]);
+  }
+
+
+  for (int i = -1 * padding; i < n ; i += strike) {
+    printf("%3d,%8f\n", count, findMax(originalInput, i, i + poolSize));
+    count++;
+  }
+
+
+  return 0;
+}
